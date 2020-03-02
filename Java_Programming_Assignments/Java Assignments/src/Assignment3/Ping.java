a1fa6279a752187ff35cf6464d8d0faff11cedff
vangaru shanmukha <vangarushanmukha@gmail.com>
2020-03-02 14:16:16 +0530

    Calculated median time and added functionality for catching the invalid or unreachable errors

--- Java_Programming_Assignments/Java Assignments/src/Assignment3/Ping.java
+++ Java_Programming_Assignments/Java Assignments/src/Assignment3/Ping.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 public class Ping {
 	@SuppressWarnings("resource")
 	public static void main(String args[]) throws IOException {
+		System.out.println("Enter the host name: ");
 		Scanner sc = new Scanner(System.in);
 		// scanning the hostname from the user
 		String hostname = sc.next();
@@ -20,7 +21,7 @@ public class Ping {
 		List<String> commands = new ArrayList<String>();
 		commands.add("ping");
 		commands.add("-c");
-		commands.add("5");
+		commands.add("4");
 		commands.add(hostname);
 		ping.doCommand(commands);
 	}
@@ -33,23 +34,33 @@ public class Ping {
 		// creating a buffered reader for reading the output produced by executing the
 		// command
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		System.out.println("Here is the standard output of the command:\n");
-		// storing the output lines in a arraylist
-		ArrayList<String> list = new ArrayList<String>();
+		BufferedReader Error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+		// storing the time in a arraylist
+		ArrayList<Double> timeList = new ArrayList<Double>();
 		while ((s = stdInput.readLine()) != null) {
-			list.add(s);
+			int timeIndex = 0;
+			if (s.contains("time=")) {
+				timeIndex = s.indexOf("time=") + 5;
+				timeList.add(Double.parseDouble(s.substring(timeIndex, s.length() - 3)));
+			}
 		}
-		// getting the line that contains the round trip time
-		String rtt = list.get(list.size() - 1);
-		// splitting the line based on = and spaces
-		String strings[] = rtt.split("[= | \\s+]");
-		// storing the key values (min,max,avg,mdev)
-		String keys[] = strings[1].split("/");
-		// storing the values for the above keys
-		String values[] = strings[4].split("/");
-		// printing the keys along with their values
-		for (int i = 0; i < keys.length; i++) {
-			System.out.println(keys[i] + " " + values[i]);
+		// code for calculating the median time
+		if (timeList.size() > 0) {
+			int n = timeList.size();
+			double median = 0;
+			if (n % 2 == 0) {
+				median = (timeList.get(n / 2) + timeList.get((n - 1) / 2)) / 2;
+			} else {
+				median = timeList.get(n / 2);
+			}
+			// printing the median time
+			System.out.println("Median time : " + median);
+		}
+		// code for printing the error messages (if any)
+		else {
+			while ((s = Error.readLine()) != null) {
+				System.out.println(s);
+			}
 		}
 	}
 }
