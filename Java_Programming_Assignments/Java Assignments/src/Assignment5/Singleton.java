be079719ea88f89079294fca7e9b5f0f33cbec24
vangaru shanmukha <vangarushanmukha@gmail.com>
2020-03-02 14:29:11 +0530

    Assignment 5

--- Java_Programming_Assignments/Java Assignments/src/Assignment5/Singleton.java
+++ Java_Programming_Assignments/Java Assignments/src/Assignment5/Singleton.java
@@ -3,10 +3,11 @@
  * Add a static method that takes String as parameter and initialize the member variable and then return object of that class. 
  * Add a non static method to print the String.
  */
-package Assignment5;
+package test;
 
 public class Singleton {
-	String string;
+	private static String string;
+	private static Singleton singleton = new Singleton();
 
 	public static Singleton staticMethod(String string1) {
 		/**
@@ -15,7 +16,7 @@ public class Singleton {
 		 * The above statement throws a compilation error because we cannot access a
 		 * non-static member from a static context.
 		 */
-		return new Singleton();
+		return singleton;
 	}
 
 	public void nonStaticMethod() {
